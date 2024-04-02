c233dc86ce5319becdf874a02c5f342a1395e919
cleciopinheiro <cleciosantosdeveloper@gmail>
2024-04-02 20:44:57 -0300

    fix? manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,7 +52,6 @@ export default function RootLayout({
         <meta charSet="utf-8" />
         <link rel="manifest" href="/site.webmanifest" />
         <meta http-equiv="Permissions-Policy" content="interest-cohort=()" />
-
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body className={inter.className}>
