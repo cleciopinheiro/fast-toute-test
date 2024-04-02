5d9024b1bf7e24cc7cdb7a5bb59b10a41c150592
cleciopinheiro <clecio.pinheiro@outlook.com>
2024-04-02 20:26:13 -0300

    manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,14 +10,34 @@ export const metadata: Metadata = {
   description: "Your bestYour best route option",
   icons: [
     {
+      url: "/16.png",
       rel: "icon",
-      href: "/favicon.ico",
-      url: "/favicon.ico", // Add the missing url property
+      type: "image/png",
+      href: "/16.png",
+      sizes: "16x16",
+    },
+    {
+      url: "/32.png",
+      rel: "icon",
+      type: "image/png",
+      href: "/32.png",
+      sizes: "32x32",
+    },
+    {
+      url: "/192.png",
+      rel: "icon",
+      type: "image/png",
+      href: "/192.png",
+      sizes: "192x192",
+    },
+    {
+      url: "/512.png",
+      rel: "icon",
+      type: "image/png",
+      href: "/512.png",
+      sizes: "512x512",
     },
   ],
-  applicationName: "Fast Route",
-  viewport: "width=device-width, initial-scale=1.0",
-  publisher: "Fast Route",
 };
 
 
@@ -28,6 +48,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta http-equiv="Permissions-Policy" content="interest-cohort=()" />
+
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <body className={inter.className}>
             <ModalProvider />
             {children}
