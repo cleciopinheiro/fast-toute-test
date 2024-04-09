289a6842b12fa465a03b85379b0221ed39e4d3fb
Jaci-Xavier <devxvr@gmail.com>
2024-04-09 20:04:00 -0300

    adiciona map baltimore

--- app/components/MapSection.tsx
+++ app/components/MapSection.tsx
@@ -1,15 +1,24 @@
-import Image from "next/image";
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 interface MapSectionProps {
-    id: string;
+  id: string;
 }
 
 function MapSection({ id }: MapSectionProps) {
-    return (
-        <div id={id} className="min-h-[79vh] w-screen">
-            <Image src="/map.webp" width={500} height={500} alt="map" className="h-[79h] w-full" />
-        </div>
-    );
+  const [station, setStation] = useState('');
+
+  useEffect(() => {
+    const st = JSON.parse(localStorage.getItem('station') || '');
+    setStation(st);
+  }, []);
+
+  return (
+    <div id={id} className="min-h-[79vh] w-screen">
+      { station === 'Springfield' && <Image src="/map.webp" width={500} height={500} alt="map" className="h-[79h] w-full" /> }
+      { station === 'Elkridge' && <Image src="/map2.webp" width={500} height={500} alt="map" className="h-[79h] w-full" /> }
+    </div>
+  );
 }
 
 export default MapSection;
--- app/layout.tsx
+++ app/layout.tsx
@@ -51,7 +51,7 @@ export default function RootLayout({
       <head>
         <meta charSet="utf-8" />
         <link rel="manifest" href="/site.webmanifest" />
-        <meta http-equiv="Permissions-Policy" content="interest-cohort=()" />
+        <meta httpEquiv="Permissions-Policy" content="interest-cohort=()" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body className={inter.className}>
